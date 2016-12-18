56515d061751fe9ca7d97a48ce6f8ae7536eb546
Aya Kahine <aya.kahihe@vub.be>
2016-12-18 12:35:08 +0100

    //

--- app/src/main/java/com/test/MainActivity.java
+++ app/src/main/java/com/test/MainActivity.java
@@ -2,15 +2,19 @@ package com.test;
 
 import android.app.Notification;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.app.Activity;
+import android.preference.PreferenceManager;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class
 MainActivity extends Activity {
+    private String gesture;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +32,7 @@ MainActivity extends Activity {
             Button b = new Button(this);
             b.setCompoundDrawablesWithIntrinsicBounds(picture,0,0,0);
             b.setText(ActionTriggers.ActionType.getDisplayName(type));
+
             b.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
@@ -39,6 +44,11 @@ MainActivity extends Activity {
             });
 
             layout.addView(b);
+
+
+            TextView t = new TextView(MainActivity.this);
+            t.setText( " gesture is connected to " + ActionTriggers.ActionType.getDisplayName(type));
+            layout.addView(t);
 
         }
 
